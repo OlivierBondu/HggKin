7072d28605dd7ce804a627e16822a017a14eaaca
cgoudet <christophe.raymond.goudet@cern.ch>
2013-06-25 16:27:03 +0200

    update param_kin.h

--- param_kin.h
+++ param_kin.h
@@ -4,5 +4,5 @@ int const windowval[n_window]={0,2,3};
 int const n_cuttheta=4;
 float const cuttheta[n_cuttheta]={0.2,0.375,0.550,0.75};
 
-int const n_kinvar=2;
-char const *kinvarval[n_kinvar]={"pt","costheta"};
+int const n_kinvar=3;
+char const *kinvarval[n_kinvar]={"pt","costheta","mass"};
